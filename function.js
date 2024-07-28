73c079ab6ff00901b19d4253ee1f3aeb43480d08
Tino <tinomhedziso21@gmail.com>
2024-07-28 17:26:33 +0200

    Update

---
+++ function.js
@@ -0,0 +1,21 @@
+/** @format */
+
+const cursorDot = document.getElementById("cursor-dot");
+const cursorOutline = document.getElementById("cursor-outline");
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  //   cursorOutline.style.left = `${posX}px`;
+  //   cursorOutline.style.top = `${posY}px`;
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    {duration: 500, fill: "forwards"}
+  );
+});
